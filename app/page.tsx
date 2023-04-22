9548fe4f5fca91602d9774b4b85e8d3281f8e6b1
issadiop2580 <issadiop2580@gmail.com>
2023-04-22 03:38:46 +0000

    fix background

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
 'use client'
+import { useState, useEffect } from 'react'
+import Box from '@mui/material/Box'
 import Body from "@/components/Body/Body"
 import Navbar from "@/components/Navbar.tsx/Navbar"
-import { Box } from "@mui/material"
+
+const images = [
+  '/images/tst1.jpg',
+  '/images/tst2.jpg',
+  '/images/tst3.jpg',
+];
 
 export default function Home() {
-  return (
 
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-        backgroundColor: '#0a192f',
-      }}>
-        <Navbar />
-        <Body />
-    </Box>
+  const bg = {
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    transition: 'background-image 1s ease-in-out',
+    minHeight: '100vh',
+  }
 
-  )
+  const [index, setIndex] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setIndex(prevIndex => (prevIndex + 1) % images.length);
+      }, 6000);
+      return () => clearInterval(interval);
+    }, []);
+
+    const backgroundImageStyle = { backgroundImage: `url(${images[index]})` };
+
+    return (
+      <Box sx={{...bg}} style={backgroundImageStyle}>
+        tsttst
+      </Box>
+    )
 }
